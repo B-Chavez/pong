522cf8e03594587c8caebe0d3ee2594d10c6b852
B-Chavez <59714905+B-Chavez@users.noreply.github.com>
2025-06-09 21:58:48 -0700

    Keep velocity consistent
    Updated ball.gd script with consistent velocity

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -5,9 +5,17 @@ extends RigidBody2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#random initial direction
-	var angle = randf() * PI * 2
-	linear_velocity = Vector2.RIGHT.rotated(angle) * speed
+	var angle = randf() * TAU #randf() returns a float 0.0 to 1.0, then you multiply it by TAU(PI * 2) which gives you 360 degrees by it self
+	#Because PI is 180 degrees
+	linear_velocity = Vector2.RIGHT.rotated(angle) * speed 
+	#Vector2.RIGHT Face your character right, then rotate in a random 360 deg
+	#then move your ball at that speed
 
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	# ensure we keep exactly 'speed' magitude
+	var lv = state.get_linear_velocity()
+	if lv != Vector2.ZERO:
+		state.set_linear_velocity(lv.normalized() * speed)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
